9258530d0ce75b69c29e319c2853efc7b8671b71
DhruvPrajapati123 <dhruvprajapati8200@gmail.com>
2021-05-28 15:46:06 +0530

    run npm build

---
+++ build/precache-manifest.8b1f18d1a311695340f2fe97ede6fb33.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a86d4caf82314a3a6dd78506bb58bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf7cbc68387f33af163",
+    "url": "/static/css/main.f039730b.chunk.css"
+  },
+  {
+    "revision": "b01dd34f6853be51fdb7",
+    "url": "/static/js/2.152896a1.chunk.js"
+  },
+  {
+    "revision": "2cf7cbc68387f33af163",
+    "url": "/static/js/main.e0962680.chunk.js"
+  },
+  {
+    "revision": "c19cbbb35f6093455bba",
+    "url": "/static/js/runtime-main.c439f095.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "1f9268e9a9d8539cb26db60ce242338e",
+    "url": "/static/media/r-1.1f9268e9.jpg"
+  },
+  {
+    "revision": "cb4283d62c0d8814462eae739d7eda6e",
+    "url": "/static/media/r-10.cb4283d6.jpg"
+  },
+  {
+    "revision": "be74857b8b74c78bcf2de70a0f340a66",
+    "url": "/static/media/r-11.be74857b.jpg"
+  },
+  {
+    "revision": "3242f67760e5b637d977e5606754851f",
+    "url": "/static/media/r-12.3242f677.jpg"
+  },
+  {
+    "revision": "3242f67760e5b637d977e5606754851f",
+    "url": "/static/media/r-3.3242f677.jpg"
+  },
+  {
+    "revision": "220244f32f58c05f855e226f9cdde16d",
+    "url": "/static/media/r-4.220244f3.jpg"
+  },
+  {
+    "revision": "ef6b16a365f9ea54a3ba40647d33bfc6",
+    "url": "/static/media/r-5.ef6b16a3.jpg"
+  },
+  {
+    "revision": "157e4ff9e44e4b709751f4feca578643",
+    "url": "/static/media/r-6.157e4ff9.jpg"
+  },
+  {
+    "revision": "488d3532269c57f88f53e28a34d71543",
+    "url": "/static/media/r-7.488d3532.jpg"
+  },
+  {
+    "revision": "448421cae5d31ce3439e82583609dc72",
+    "url": "/static/media/r-8.448421ca.jpg"
+  },
+  {
+    "revision": "a45280b70811ad5b8cafeabf42c781cb",
+    "url": "/static/media/r-9.a45280b7.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
--- src/context.js
+++ src/context.js
@@ -109,7 +109,7 @@ export default class RoomProvider extends Component {
 
     // filter by type
     if (type !== "all") {
-      tempRooms = tempRooms.filter((room) => room.type == type);
+      tempRooms = tempRooms.filter((room) => room.type === type);
     }
 
     // filter by capacity
