831f922eeac29addc90bb53daefe183149718400
DhruvPrajapati123 <dhruvprajapati8200@gmail.com>
2021-05-31 11:36:36 +0530

    Initial commit

--- src/Contentfull.js
+++ src/Contentfull.js
@@ -1,6 +1,8 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  // space: process.env.REACT_APP_API_SPACE,
+  // accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: "630spzdmi6wu",
+  accessToken: "Wx1032kkSDTS9ef4ptaBfa8jywvW0L4FBnNSrQ0aCkY",
 });
